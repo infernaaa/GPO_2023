fa38dc76a72a55b0ea51bcf87939aecb89dfcb35
infernaaa <spak.ich@yandex.ru>
2023-05-18 12:12:50 +0700

    Init commit

---
+++ Model_data/MainWindow.xaml.cs
@@ -0,0 +1,108 @@
+﻿using ScottPlot;
+using ScottPlot.Plottable;
+using ScottPlot.WPF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Model_data
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private readonly ScottPlot.Plottable.MarkerPlot HighlightedPoint;
+        private int LastHighlightedIndex = -1;
+        
+        private readonly ScottPlot.Plottable.ScatterPlot MyScatterPlot;
+
+        
+
+        private void ClearPlot(object sender, RoutedEventArgs e)
+        {
+            wpf_plot.Plot.Clear();
+            wpf_plot.Plot.AxisAuto();
+        }
+
+        private void DeployCustomMenu(object sender, EventArgs e)
+        {
+
+
+            MenuItem clearPlotMenuItem = new MenuItem() { Header = "Clear Plot" };
+            clearPlotMenuItem.Click += ClearPlot;
+
+            ContextMenu rightClickMenu = new ContextMenu();
+
+            rightClickMenu.Items.Add(clearPlotMenuItem);
+
+            rightClickMenu.IsOpen = true;
+        }
+        private void formsPlot1_MouseMove(object sender, MouseEventArgs e)
+        {
+
+            // determine point nearest the cursor
+            (double mouseCoordX, double mouseCoordY) = wpf_plot.GetMouseCoordinates();
+            double xyRatio = wpf_plot.Plot.XAxis.Dims.PxPerUnit / wpf_plot.Plot.YAxis.Dims.PxPerUnit;
+            (double pointX, double pointY, int pointIndex) = MyScatterPlot.GetPointNearest(mouseCoordX, mouseCoordY, xyRatio);
+
+            // place the highlight over the point of interest
+            HighlightedPoint.X = pointX;
+            HighlightedPoint.Y = pointY;
+            HighlightedPoint.IsVisible = true;
+
+            // render if the highlighted point chnaged
+            if (LastHighlightedIndex != pointIndex)
+            {
+                LastHighlightedIndex = pointIndex;
+                wpf_plot.Render();
+            }
+
+            // update the GUI to describe the highlighted point
+            //TextBox_File_Name.Text =pointIndex.ToString();
+            TextBox_Number_Detector.Text = pointX.ToString();
+            TextBox_Number_Str.Text = pointY.ToString();
+            //Console.WriteLine($"Point index {pointIndex} at ({pointX:N2}, {pointY:N2})");
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+            // create a scatter plot from some random data and save it
+            double[] dataX = new double[] { 1, 2, 3, 4, 5 };
+            double[] dataY = new double[] { 1, 4, 9, 16, 25 };
+            MyScatterPlot = wpf_plot.Plot.AddScatter(dataX, dataY);
+
+
+
+            // Add a red circle we can move around later as a highlighted point indicator
+            HighlightedPoint = wpf_plot.Plot.AddPoint(0, 0);
+            HighlightedPoint.Color = System.Drawing.Color.Red;
+            HighlightedPoint.MarkerSize = 10;
+            HighlightedPoint.MarkerShape = ScottPlot.MarkerShape.openCircle;
+            HighlightedPoint.IsVisible = false;
+            wpf_plot.MouseMove += formsPlot1_MouseMove;
+            wpf_plot.RightClicked += DeployCustomMenu;
+            wpf_plot.Refresh();
+
+
+
+
+        }
+    }
+}
---
+++ Preprocess_data/MainWindow.xaml.cs
@@ -0,0 +1,234 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Diagnostics;
+using Preprocess_data.Properties;
+
+namespace Preprocess_data
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+        }
+        private void BrowseButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Файлы CSV|*.csv";
+            if (openFileDialog.ShowDialog() == true)
+            {
+
+                FilePathTextBox.Text = openFileDialog.FileName;
+            }
+        }
+
+        private void StartProccessBTM_Click(object sender, RoutedEventArgs e)
+        {
+
+
+            if (IsStringPath(FilePathTextBox.Text))
+            {
+                if (!File.Exists(FilePathTextBox.Text))
+                {
+                    MessageBox.Show("Файла не существует");
+
+                }
+                else
+                {
+                    
+
+
+                    if (option1.IsChecked == true)
+                    {
+
+                        input_data(FilePathTextBox.Text);
+                        MessageBox.Show(FilePathTextBox.Text);
+                    }
+                    else if (option2.IsChecked == true)
+                    {
+
+                       output_data(FilePathTextBox.Text);
+                    }
+
+
+                }
+            }
+
+        }
+        private bool IsStringPath(String path)
+        {
+            // Проверка, является ли поданная в функцию строка путем 
+            // к файлу или папке
+            Regex regex = new Regex(@"[a-zA-Z]:(\\.+)+(.csv\b)");
+            path = regex.Match(path).Value;
+            if (path != "")
+            {
+
+                return true;
+            }
+            MessageBox.Show("путь нулевой");
+            return false;
+        }
+
+        private void Window_Initialized(object sender, EventArgs e)
+        {
+            inst.Text = "Шаг 1. Нажмите на кнопку <Обзор>. В открывшемся окне найдите нужную папку, в которой выберите интересующий файл с расширением .csv и нажмите кнопку <Открыть>. После выбора необходимого файла для обработки путь к нему отобразится в текстовом поле <Путь к обрабатываемому файлу>." +
+"В открывшемся окне будут показаны только файлы с расширением.csv, файлы иного расширения не будут отображены\n" +
+                "Шаг 2. Из списка, расположенного ниже, необходимо выбрать нужный тип данных: входные или выходные. Можно выбрать только один вариант из предложенных.Выбор другого варианта приведёт к отмене варианта, который был выбран до этого действия.\n" +
+                "Шаг 3. Нажмите на кнопку <Обработать файл>. Появится всплывающие окно, что данные успешно обработаны." +
+" Если в текстовом поле <Путь к обрабатываемому файлу> допущена ошибка в написании названия, пути или расширения обрабатываемого файла, то выйдет окно с оповещением об ошибке.В таком случае необходимо повторить шаги для выбора файла для обработки данных(назад к шагу 1).\n" +
+                "Шаг 4. После обработки файла откроется окно с графиком, представляющий обработанные данные." +
+"Для того чтобы передвинуть файл по горизонтали и / или вертикали необходимо навести курсор мыши на график и зажать левую кнопку мыши." +
+" Для того чтобы изменить масштаб графика необходимо навести курсор мыши на график, прокручивая колёсико мыши, выбрать подходящий масштаб.\n";
+        }
+
+        /*
+         private void input_data(string path)
+         {
+
+             MessageBox.Show($"Успешно, входные данные путь к файлу {path}");
+         }
+         private void output_data(string path)
+         {
+             MessageBox.Show($"Успешно, выходные данные данные путь к файлу {path}");
+         }
+        */
+
+        private void input_data(string path)
+        {
+            // выполнение кода, когда выбрана кнопка "Входные данные"
+            //////////////////////////// КОНСТАТЫ ДЛЯ PYTHON АЛГОРИТМА
+            // путь к файлу для обработки
+            String path_to_data = path;// сделать путь к файлу
+            // путь для сохранения обработанного файла
+            String path_to_save_data_array_like = "\"C:\\Users\\infernaaa\\Desktop\\X_data.xlsx\"";
+
+
+            //////////////////////////// ОСТАЛЬНОЕ
+
+            // путь, куда сохранять алгоритм из ресурсов
+            //String X_preprocess_file_path = "X_preprocess.py";
+
+            // Переменная, хранящая всю инфу о файле python алгоритма
+            //FileInfo X_preprocess = new FileInfo(X_preprocess_file_path);
+
+            // Сохранение python алгоритма из ресурсов в файл
+            //File.WriteAllBytes(X_preprocess_file_path, Resources.prepare_X_data);//prepare_X_data.py
+
+            // Проверка, сохранился ли файл
+            if (!File.Exists("Python_scripts//preprocess_X_data.py"))
+            {
+
+                Console.WriteLine("Ошибка создания скрипта обработки X выборки");
+                return;
+            }
+
+            // Запуск алгоритма обработки в python
+            try
+            {
+                Process process = new Process();
+                ProcessStartInfo startInfo = new ProcessStartInfo();
+                startInfo.FileName = "CMD.exe";
+                startInfo.WorkingDirectory = "Python_scripts";
+                startInfo.Arguments = "/C python " + "preprocess_X_data.py" +
+                                                    " " + path_to_data +
+                                                    " " + path_to_save_data_array_like;
+                process.StartInfo = startInfo;
+                process.StartInfo.RedirectStandardOutput = true;
+                process.StartInfo.UseShellExecute = false;
+                process.Start();
+
+                process.WaitForExit();
+
+                MessageBox.Show(process.StandardOutput.ReadToEnd());
+
+                
+            }
+            catch (Exception e_i)
+            {
+                MessageBox.Show(e_i.Message);
+            }
+        }
+
+        private void output_data(string path)
+        {
+            // выполнение кода, когда выбрана кнопка "Выходные данные"
+            //////////////////////////// КОНСТАТЫ ДЛЯ PYTHON АЛГОРИТМА
+            // путь к файлу для обработки
+            String path_to_defects = path;
+            // путь для сохранения обработанного файла
+            String path_to_save_defects = "\"C:\\Users\\infernaaa\\Desktop\\Y_data.xlsx\"";
+
+            // для Run1 115
+            // для Run2 119
+            int rows_number = 115;
+            int detectors_number = 400;
+
+            //////////////////////////// ОСТАЛЬНОЕ
+
+            // путь, куда сохранять алгоритм из ресурсов
+           // String Y_preprocess_file_path = "Y_preprocess.py";
+
+            // Переменная, хранящая всю инфу о файле python алгоритма
+           // FileInfo Y_preprocess = new FileInfo(Y_preprocess_file_path);
+
+            // Сохранение python алгоритма из ресурсов в файл
+            //File.WriteAllBytes(Y_preprocess_file_path, Resources.prepare_Y_data.py);//prepare_Y_data.py
+
+            // Проверка, сохранился ли файл
+            if (!File.Exists("Python_scripts//preprocess_Y_data.py"))
+            {
+                Console.WriteLine("Ошибка создания скрипта обработки X выборки");
+                return;
+            }
+
+            // Запуск алгоритма обработки в python
+            try
+            {
+                Process process = new Process();
+                ProcessStartInfo startInfo = new ProcessStartInfo();
+                startInfo.FileName = "CMD.exe";
+                startInfo.WorkingDirectory = "Python_scripts";
+                startInfo.Arguments = "/C python " + "preprocess_Y_data.py" +
+                                                    " " + path_to_defects +
+                                                    " " + path_to_save_defects +
+                                                    " " + rows_number +
+                                                    " " + detectors_number;
+                process.StartInfo = startInfo;
+                process.StartInfo.RedirectStandardOutput = true;
+                process.StartInfo.UseShellExecute = false;
+                process.Start();
+
+                Console.WriteLine(process.StandardOutput.ReadToEnd());
+
+                process.WaitForExit();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+        
+        
+    }
+}
